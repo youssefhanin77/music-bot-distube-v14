064e0edce3ee4b4118cba1ab7f0c08fb6832ffe9
youssefhanin77 <99509548+youssefhanin77@users.noreply.github.com>
2024-03-30 00:29:40 +0000

    Update config.js

--- settings/config.js
+++ settings/config.js
@@ -112,7 +112,7 @@ const numberEmojis = [
 
 const slashSettings = {
   global: false,
-  guildIDS: ["1108474287389675591"], // for slash command
+  guildIDS: ["1213280679102775347"], // for slash command
 };
 
 module.exports = {
